7224702f554a8ec663614932314432c401556ece
Iain Tooley <iaintooley1@gmail.com>
2019-10-18 14:00:10 +0100

    added form submitions

--- js/app.js
+++ js/app.js
@@ -6,11 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const handleNewAnimalFormSubmit = function (event) {
-  event.preventDefult();
+  event.preventDefault();
 
   const newListAnimal = createNewListAnimal(event.target);
   const listAnimal = document.querySelector("#endangered-animals");
   listAnimal.appendChild(newListAnimal);
 
   event.target.reset();
+}
+
+const createNewListAnimal = function (form) {
+  const newListAnimal = document.createElement("div");
+
+  const name = document.createElement("h2");
+  name.textContent = form.name.value;
+  newListAnimal.appendChild(name);
+
+  const species = document.createElement("h3");
+  species.textContent = form.species.value;
+  newListAnimal.appendChild(species);
+
+  const continent = document.createElement("p");
+  continent.textContent = form.continent.value;
+  newListAnimal.appendChild(continent);
+
+  return newListAnimal;
 }
